ef3bbb56030c4665ef67a736e496c3eff0808412
Roland Hart <rolandhart@Rolands-MacBook-Pro.local>
2025-06-13 15:08:53 -0400

    Add test route to send Hargrove and Didion messages

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 from flask import Flask, request
+from twilio.rest import Client
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load .env variables
 
 app = Flask(__name__)
+
+# Twilio setup
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+messaging_service_sid = os.environ.get("MESSAGING_SERVICE_SID")
+client = Client(account_sid, auth_token)
 
 @app.route("/")
 def home():
     return "Cat bot is running!"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render provides PORT
-    app.run(host="0.0.0.0", port=port)
+@app.route("/test")
+def send_test_messages():
+    recipient = "+13193892071"
+
+    client.messages.create(
+        to=recipient,
+        messaging_service_sid=messaging_service_sid,
+        body="i ate a sock. it was crunchy - hargs"
+    )
+
+    client.messages.create(
+        to=recipient,
+        messaging_service_sid=messaging_service_sid,
+        body="she ate a sock. i cried - dids"
+    )
+
+    return "Test messages sent!"
